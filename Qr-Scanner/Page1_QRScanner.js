7595c3e01e4c6f33e8a5a2d140b5192b6e015a4d
BUE-LJ-BT <41939168+BUE-LJ-BT@users.noreply.github.com>
2018-09-17 10:16:13 +0200

    Add files via upload

--- Qr-Scanner/Page1_QRScanner.js
+++ Qr-Scanner/Page1_QRScanner.js
@@ -1,9 +1,7 @@
-﻿const QREnableButton = document.getElementById('qr-Button');
+﻿import QrScanner from "./qr-scanner.min.js";
+
+const QREnableButton = document.getElementById('qr-Button');
 const QRAll = document.getElementById('qr-Container');
-
-
-import QrScanner from "./qr-scanner.min.js";
-
 const video = document.getElementById('qr-video');
 const camQrResult = document.getElementById('cam-qr-result');
 
@@ -14,8 +12,18 @@ function setResult(label, result) {
     label.highlightTimeout = setTimeout(() => label.style.color = 'inherit', 100);
 }
 
-
 // ####### Web Cam Scanning #######
-
 const scanner = new QrScanner(video, result => setResult(camQrResult, result));
-scanner.start();
+
+
+QREnableButton.addEventListener('click', () => {
+    if (QRAll.hidden == true) {
+        QRAll.hidden = false;
+        QREnableButton.innerHTML = 'Close QR Scanner';
+        scanner.start();
+    } else {
+        QRAll.hidden = true;
+        QREnableButton.innerHTML = 'Use QR Scanner';
+        scanner.stop();
+    }
+});
