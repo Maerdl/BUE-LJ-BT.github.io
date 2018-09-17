bd2e8309e083b0dfee0cea1e30659c03c1b8e691
BUE-LJ-BT <41939168+BUE-LJ-BT@users.noreply.github.com>
2018-09-17 08:41:17 +0200

    Add files via upload

---
+++ Qr-Scanner/Page1_QRScanner.js
@@ -0,0 +1,21 @@
+﻿const QREnableButton = document.getElementById('qr-Button');
+const QRAll = document.getElementById('qr-Container');
+
+
+import QrScanner from "./qr-scanner.min.js";
+
+const video = document.getElementById('qr-video');
+const camQrResult = document.getElementById('cam-qr-result');
+
+function setResult(label, result) {
+    label.textContent = result;
+    label.style.color = 'teal';
+    clearTimeout(label.highlightTimeout);
+    label.highlightTimeout = setTimeout(() => label.style.color = 'inherit', 100);
+}
+
+
+// ####### Web Cam Scanning #######
+
+const scanner = new QrScanner(video, result => setResult(camQrResult, result));
+scanner.start();
